01c685d359c374c90ce0b3e29283a6e825e09172
Daniel Shamburger <daniel@wearetbx.com>
2019-01-24 16:04:25 -0500

    2019 site v1

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+		
+	// http://stackoverflow.com/questions/7717527/jquery-smooth-scrolling-when-clicking-an-anchor-link/7717572#7717572?newreg=16ca424bc4024b21a4fcc728ea6451d5
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 500);
+	    return false;
+	});
+
+	var menuIsOpen = false;
+
+	$('.menu-toggle').click(function() {
+		$('nav').slideToggle("fast");
+		if(!menuIsOpen) {
+			$('.menu-toggle').text("CLOSE");
+			menuIsOpen = true;
+		} else {
+			$('.menu-toggle').text("MENU");
+			menuIsOpen = false;
+		}
+	});
+
+});
